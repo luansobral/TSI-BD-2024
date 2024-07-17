bc3e14f132dda2037f906ab3a5784b57f0f18619
luansobral <luangabrielsobral160804@gmail.com>
2024-07-16 21:59:16 -0300

    commit 5

--- Aula1/Exemplo1.sql
+++ Aula1/Exemplo1.sql
@@ -5,16 +5,35 @@ CREATE DATABASE IFSP;
 USE IFSP;
 
 -- CRIAR TABELA CIDADE
-CREATE TABLE CIDADE(id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(45) NOT NULL, estado CHAR(2) NOT NULL);
+CREATE TABLE CIDADE(id INT NOT NULL AUTO_INCREMENT, nome VARCHAR(45) NOT NULL, estado CHAR(2) NOT NULL, PRIMARY KEY (id));
 
 -- CRIAR TABELA CLIENTE
 CREATE TABLE CLIENTE(
-    id INT PRIMARY KEY AUTO_INCREMENT, 
+    id INT NOT NULL AUTO_INCREMENT, 
     nome VARCHAR(45) NOT NULL, 
     email VARCHAR(55) NOT NULL,
-    CONSTRAINT fk_cliente_cidade FOREIGN KEY (id_cidade) REFERENCES CIDADE (id),
     endereco VARCHAR(45),
     bairro VARCHAR(45),
     numero INT,
-    data_nasc DATE,
+    data_nasc DATETIME,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_cliente_cidade FOREIGN KEY(id_cidade) REFERENCES CIDADE(id)
     );
+
+-- INSERIR DADOR NA TABELA CIDADE
+INSERT INTO CIDADE(id, nome, estado) VALUES (1, 'Birigui', 'SP');
+INSERT INTO CIDADE(id, nome, estado) VALUES (2, 'Aracatuba', 'SP');
+INSERT INTO CIDADE(nome, estado) VALUES ('Guararapes', 'SP'); -- Nâo precisa falar o valor do id quando é auto_increment
+INSERT INTO CIDADE(nome, estado) VALUES ('Rio de Janeiro', 'RJ');
+
+-- MOSTRA REGISTROS NA TABELA CIDADE
+SELECT * FROM CIDADE;
+
+-- INSERIR DADOS NA TABELA CLIENTE
+INSERT INTO CLIENTE(nome, email, id_cidade, endereco, bairro, numero, data_nasc) VALUES ('Luan Sobral', 'luan@mail.com', 1, 'Rua Bonita', 'Jandaia 6', 180, '2004/08/16');
+
+INSERT INTO CLIENTE(nome, email, id_cidade, endereco, bairro, numero, data_nasc) VALUES ('Dono da blaze', 'blaze@mail.com', 4,
+'Rua Estanha', 'Belford Roxo', 10, '1804/08/16');
+
+-- MOSTRA REGISTROS NA TABELA CLIENTE
+SELECT * FROM CLIENTE;
